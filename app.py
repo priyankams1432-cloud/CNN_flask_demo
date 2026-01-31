a2d00902f4a773d6170ffa1847689732322ec902
priyankams1432-cloud <priyankams1432@gmail.com>
2026-01-31 12:16:02 +0530

    commit first

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
+
+@app.route("/abc")
+def abc():
+    return render_template("abc.html")
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=5000)
